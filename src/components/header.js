70653cffe24986a999bd73cd097d301f7bc99c71
Nakul Balaiwar <gnakul46@gmail.com>
2023-10-06 11:40:27 +0530

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
+
+const header = () => {
+  return <>
+    <header className='header-top-strip'>
+      <div className='container-xxl'></div>
+    </header>
+  </>
+}
+
+export default header
