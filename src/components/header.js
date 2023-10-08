7a017b3954657fd7fcbdff645d8394eb27ae313b
Nakul Balaiwar <gnakul46@gmail.com>
2023-10-08 23:17:47 +0530

    update

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,21 @@ import { NavLink, Link } from 'react-router-dom'
 
 const header = () => {
   return <>
-    <header className='header-top-strip'>
-      <div className='container-xxl'></div>
+    <header className='header-top-strip py-3'>
+      <div className='container-xxl'>
+        <div className="row">
+          <div className="col-6">
+            <p className='text-white mb-0'>
+              Free Shipping Over $100 & Free Returns
+            </p>
+          </div>
+          <div className="col-6">
+            <p className='text-end text-white'>
+              Hotline: <a className="text-white" href='tel:+91 6392307525'>+91 6392307525</a>
+            </p>
+          </div>
+        </div>
+      </div>
     </header>
   </>
 }
